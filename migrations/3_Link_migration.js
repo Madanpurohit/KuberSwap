9998ff061d09840f7e5555f675f00a06f43ced4b
Madan Purohit <mspurohit09@gmail.com>
2021-03-16 16:05:56 +0530

    improving migration

--- migrations/3_Link_migration.js
+++ migrations/3_Link_migration.js
@@ -1,5 +1,13 @@
-const Migrations = artifacts.require("Link");
+const Link = artifacts.require("Link");
+const Wallet=artifacts.require("Wallet");
 
-module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+module.exports = async function (deployer,network,accounts) {
+  await deployer.deploy(Link);
+  let wallet=await Wallet.deployed();
+  let link=await Link.deployed();
+  await link.approve(wallet.address,500);
+  await wallet.addToken(web3.utils.fromUtf8(link.symbol()),link.address);
+  await wallet.Deposit(web3.utils.fromUtf8(link.symbol()),100);
+  let balance=await wallet.balances(accounts[0],web3.utils.fromUtf8(link.symbol()));
+  console.log("balance is :",balance);
 };
